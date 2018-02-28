dda4e20d8168e56b57f2764ff0ad8a4dfba82c37
Dan Castanera <dcastanera@gmail.com>
2018-02-28 03:46:30 -0500

    update

--- src/RolesServiceProvider.php
+++ src/RolesServiceProvider.php
@@ -19,9 +19,12 @@ class RolesServiceProvider extends ServiceProvider
          * This copies the migrations for the roles and permissions tables to the
          * default database/migrations directory.
          */
-        $this->publishes([
-            __DIR__.'/migrations' => base_path('database/migrations')
-        ]);
+
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+
+        // $this->publishes([
+        //     __DIR__.'/migrations' => base_path('database/migrations')
+        // ]);
 
         /**
          * Role Blade variable.
